383bd7105c51299aa77611b9d95604ff70ca561f
cuihongkai123 <2398865146@qq.com>
2021-08-11 11:47:26 +0800

    42r423

--- src/test/AAA.java
+++ src/test/AAA.java
@@ -5,28 +5,29 @@ public class AAA {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("aaaaa");
-		System.out.println("更新1");
-		System.out.println("更新2");
-		System.out.println("更新3");
-		System.out.println("新的分支  new branch");
-		System.out.println("主线添加");
-		System.out.println("分支 添加");
-		System.out.println("分支 再添加");
-		System.out.println("分支 再再添加");
-		System.out.println("主线 再添加");
-		System.out.println("第二次更新之前");
-		System.out.println("github上的！！！！！！！！！！！");
-		System.out.println("主线b上的！！！！！！！！！！！");
+		System.out.println("鏇存柊1");
+		System.out.println("鏇存柊2");
+		System.out.println("鏇存柊3");
+		System.out.println("鏂扮殑鍒嗘敮  new branch");
+		System.out.println("涓荤嚎娣诲姞");
+		System.out.println("鍒嗘敮 娣诲姞");
+		System.out.println("鍒嗘敮 鍐嶆坊鍔�");
+		System.out.println("鍒嗘敮 鍐嶅啀娣诲姞");
+		System.out.println("涓荤嚎 鍐嶆坊鍔�");
+		System.out.println("绗簩娆℃洿鏂颁箣鍓�");
+		System.out.println("github涓婄殑锛侊紒锛侊紒锛侊紒锛侊紒锛侊紒锛�");
+		System.out.println("涓荤嚎b涓婄殑锛侊紒锛侊紒锛侊紒锛侊紒锛侊紒锛�");
 
-		System.out.println("主线b上的！！！！！！！！！！！");
+		System.out.println("涓荤嚎b涓婄殑锛侊紒锛侊紒锛侊紒锛侊紒锛侊紒锛�");
 
 
-		System.out.println("github上的！！！！！！啊啊啊啊啊啊！！！！！");
+		System.out.println("github涓婄殑锛侊紒锛侊紒锛侊紒鍟婂晩鍟婂晩鍟婂晩锛侊紒锛侊紒锛�");
 
-		System.out.println("github上的！！1111111111111111！！！！！！！！！");
+		System.out.println("github涓婄殑锛侊紒1111111111111111锛侊紒锛侊紒锛侊紒锛侊紒锛�");
 
-		System.out.println("github的！都有阿布！！！！！！！！！！");
-		System.out.println("wo shi kelong shangchuan？？？？？？？？？？？？？？？？？？？？？？？？？？");
+		System.out.println("github鐨勶紒閮芥湁闃垮竷锛侊紒锛侊紒锛侊紒锛侊紒锛侊紒");
+		System.out.println("wo shi kelong shangchuan锛燂紵锛燂紵锛燂紵锛燂紵锛燂紵锛燂紵锛燂紵锛燂紵锛燂紵锛燂紵锛燂紵锛燂紵锛燂紵");
+		System.out.println("wyhjgfthd燂紵锛燂紵锛燂紵锛燂紵");
 
 	}
 
